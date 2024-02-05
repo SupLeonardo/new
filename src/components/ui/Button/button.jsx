4e28801bdef858741632f994789d26ed68251c7f
SupLeonardo Meow <levabaluevvv@gmail.com>
2024-02-05 15:28:17 +0000

    fuck your mum

--- src/components/ui/Button/button.jsx
+++ src/components/ui/Button/button.jsx
@@ -13,7 +13,7 @@ const Button = ({text, setText, cls}) => {
         }
     }
     
-    let f = 'font-bold text-white text-lg rounded-full h-[10vh] w-[10vh] my-1 mx-1 ' + cls
+    let f = 'text-white text-2xl rounded-full h-[10vh] w-[10vh] my-1 mx-1 ' + cls
     return(
         <button onClick={click} className={f}>
             {text}
